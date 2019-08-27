f3c54d9135dae5ed4007537c63840df4fd33a5ba
Olufunke Olaleye <49927264+ofunkey@users.noreply.github.com>
2019-08-27 17:05:23 -0400

    Modified app.py and scarpe_mars.py

--- Mission to Mars/app.py
+++ Mission to Mars/app.py
@@ -2,64 +2,52 @@
 ## Step 2 - MongoDB and Flask Application
 
 # Import Dependencies 
-from flask import Flask, redirect, render_template  
-import pymongo
+from flask import Flask, redirect, render_template, jsonify
+from flask_pymongo import PyMongo
 import scrape_mars
 import time
 
 # Create an instance of Flask app
 app = Flask(__name__)
 
-# Create connection with MongoDB
-conn = 'mongodb://localhost:27017'
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Pass connection to mongo instance
-client = pymongo.MongoClient(conn)
+#Can try this
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app_app"
+#mongo = PyMongo(app)
 
-# Create mars_Data Database
-db = client.mars_db
 
-# Drops collection if available to remove duplicates
-db.collection.drop()
-
-# Create mars_Data collection
-collection = db.mars
 
 # Create route that renders index.html template and finds documents from mongo
 @app.route("/")
 def index(): 
-        ######################
-        #db.collection.drop()
-        #news_dict = db.collection.find()
-        #return render_template("index.html", news_dict = news_dict)
+ 
+    # Find data
+    mars_info = mongo.db.collection.find_one()
+    print(mars_info)
+    print("did you get here")
 
-
-        mars_info = db.collection.find()
-        for info in mars_info:
-                #Return template and data
-                return render_template("index.html", mars_info = info)
+    # Return template and data
+    return render_template("index.html", mars_web=mars_info)
 
 
 # Route that will trigger scrape function
 
 @app.route("/scrape")
-def scraper():
-                
-        ####################
-        #news_dict = scrape_mars.scrape_mars_news()
-        #db.collection.insert_one(news_dict)
-        #########################
+def scrape():
 
-        #############
-        # Run the scrape function
-        mars_data = scrape_mars.scrape_info()
-        
-        # Update the Mongo database using update and upsert=True
-        #db.collection.update({}, mars_data, upsert=True)
-        db.collection.insert(mars_data)
-        #############
-        #return redirect("/", code = 302)
-        return redirect("/")
+        # Run scrapped functions
+
+        mars_info = mongo.db.collection
+        print("did you get the data?")
+        mars_data = scrape_mars.scrape_mars_info()
+
+        print(mars_info)
+
+        mars_info.update({}, mars_data, upsert=True)
+        return redirect("/", code=302)
+
 
 
 if __name__ == "__main__": 
